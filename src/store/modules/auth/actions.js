255e56868e3d1fc8c25c82970c8693bdc6e0a505
taimorkk <taimorkkr@gmail.com>
2021-04-04 23:50:33 +0500

    tokenization and auto logout on token expiration and child routes added

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,42 +1,101 @@
 
 import axios from 'axios'
+
+
+let timer;
+
 export default{
    async login( context, payload){
-   const response = await axios.post('http://droidom110-001-site1.ftempurl.com/api/Users/Login', {
-        
-          userName: payload.userName,
-          password: payload.password
-       
-      });
-
-      const responseData = await response;
-      
-        if(!responseData.data.isError){
-         
-   console.log(responseData);
-   console.log(responseData.data.model.accessToken,response.data.model.userInfo.id,response.data.model.accessTokenExpiry)
-      context.commit('setUser', {
-        token: responseData.data.model.accessToken,
-        userId: responseData.data.model.userInfo.id,
-        roleId: responseData.data.model.userInfo.roleId,
-        tokenExpiration: responseData.data.model.accessTokenExpiry
-      });
-
-        }
+  return context.dispatch('auth',{
+     ...payload,
+   })
         
       }, 
       
   
+  async auth(context ,payload){
+
    
+      const response = await axios.post('http://droidom110-001-site1.ftempurl.com/api/Users/Login', {
+           
+             userName: payload.userName,
+             password: payload.password
+          
+         });
+    
+         const responseData = await response;
+
+        
+        const expiresIn = +responseData.data.model.accessTokenExpiry * 1000;
+        
+        const expirationDate = new Date().getTime() + expiresIn;
+
+          
+
+            localStorage.setItem('token', responseData.data.model.accessToken);
+            localStorage.setItem('userId', responseData.data.model.userInfo.id);
+            localStorage.setItem('tokenExpiration', expirationDate);
+
+           timer = setTimeout(function(){
+              context.dispatch('autoLogout');
+            },expiresIn);
+
+           context.commit('setUser', {
+           token: responseData.data.model.accessToken,
+           userId: responseData.data.model.userInfo.id,
+           roleId: responseData.data.model.userInfo.roleId,
+           
+         });
    
+           
+      
+   },
+   
+   autoLogin(context){
+
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration -new Date().getTime();
+
+    if (expiresIn < 0){
+      return;
+    }
+
+    timer = setTimeout(function(){
+      context.dispatch('autoLogout');
+    },expiresIn);
+
+    if(token && userId){
+
+      context.commit('setUser',{
+        token:token,
+        userId:userId
+        
+      });
+    }
+   },
 
    logout(context){
+
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+
+
+clearTimeout(timer);
+   
        context.commit('setUser',{
 
            token:null,
-           userId:null,
-           tokenExpiration:null
+           userId:null
         });
+   },
+
+   autoLogout(context){
+     context.dispatch('logout');
+     context.commit('setAutoLogout');
    }
 
 }
